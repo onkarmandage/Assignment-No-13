dca950c8a3e492c449297010f5d347e6551eb39c
onkarmandage <onkarmandage@gmail.com>
2026-07-17 13:46:03 +0530

    Add files via upload

---
+++ Assignment13-5.py
@@ -0,0 +1,9 @@
+mark=int(input("Enter the mark: "))
+if(mark>=75):
+    print("Distiction")
+elif(mark>=60):
+    print("First class")
+elif(mark>=50):
+    print("second class")
+else:
+    print("fail")
