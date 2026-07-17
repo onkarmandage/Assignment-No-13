d5bd4fdc738551d00eee69cd97ac096927e0d1a5
onkarmandage <onkarmandage@gmail.com>
2026-07-17 13:44:04 +0530

    Add files via upload

---
+++ Assignment13-1.py
@@ -0,0 +1,8 @@
+def Area(l,b):
+    Ans=l*b
+    print("Area of ractangle is ",Ans)
+length=float(input("Enter the length "))
+width=float(input("Enter the width "))
+
+if __name__=="__main__":
+    Area(length,width)
